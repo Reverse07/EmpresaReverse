41fb7319aa33be9d5303ef4d69cc4373ece70cf9
latecnologiaavanza <cramirezchristian77@gmail.com>
2025-09-02 23:14:44 -0500

    cambios

--- empresa-reverse-frontend/src/app/features/servicios/servicios-page/servicios-page.component.ts
+++ empresa-reverse-frontend/src/app/features/servicios/servicios-page/servicios-page.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Servicio } from '../../../models/servicio.model';
 import { ServicioService } from '../../../services/servicio.service';
 import { ServiceCardComponent } from '../../../shared/service-card/service-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-servicios-page',
-  imports: [ServiceCardComponent],
+  imports: [ServiceCardComponent, CommonModule],
   templateUrl: './servicios-page.component.html',
   styleUrl: './servicios-page.component.css'
 })

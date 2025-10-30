d3ca32600f28a127fad08c3850b08ed4546acb38
latecnologiaavanza <cramirezchristian77@gmail.com>
2025-10-29 23:08:02 -0500

    mision, devs y nosotros

--- empresa-reverse-frontend/src/app/app.routes.ts
+++ empresa-reverse-frontend/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ServiciosPageComponent } from './features/servicios/servicios-page/servicios-page.component';
 import { PlanesPageComponent } from './features/planes/planes-page/planes-page.component';
+import { NosotrosComponent } from './features/nosotros/nosotros.component';
+import { MisionVisionComponent } from './features/mision-vision/mision-vision.component';
+import { DevsComponent } from './features/devs/devs.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'servicios', component: ServiciosPageComponent },
-  { path: 'planes', component: PlanesPageComponent }
+  { path: 'planes', component: PlanesPageComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'mision-vision', component: MisionVisionComponent },
+  { path: 'devs', component: DevsComponent}
 ];
